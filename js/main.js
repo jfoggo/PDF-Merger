a293f90b5a588916b63d454b3e98476efb107af2
jfoggo <49843923+jfoggo@users.noreply.github.com>
2019-08-08 09:50:34 +0200

    Page Functionality
    - added onclick event for navbar buttons
    - added side headings
    - code commented

--- js/main.js
+++ js/main.js
@@ -1,48 +1,24 @@
-var info = {
-	c1 : {
-		pdf: undefined,
-		canvas: undefined,
-		page: 1,
-		room: 1
-	},
-	c2 : {
-		pdf: undefined,
-		canvas: undefined,
-		page: 1,
-		room: 1
-	}
-};
-function load_pdf(idx,url){
-	pdfjsLib.getDocument(url).then(pdf => {
-		info[idx].pdf = pdf;
-		info[idx].page = 1;
-		info[idx].zoom = 1;
-		render(idx);
-	});
-}
-function render(idx){
-	var pdf = info[idx].pdf.then(page => {
-		var viewport = page.getViewport(info[idx].zoom);
-		info[idx].canvas.width = viewport.width;
-		info[idx].canvas.height = viewport.height;
-		page.render({
-			canvasContext: canvas.getContext("2d"),
-			viewport: viewport
-		});
-	});
-}
 function resize_css(){
 	$("#side").outerHeight($(window).innerHeight()-$("#head").outerHeight());
 }
 function init(){
+	// Initialize Page
 	resize_css();
-	$("#body").animate({opacity:1},500);
 	$(window).resize(resize_css);
-	/*
-	var canvas = $("canvas");
-	info.c1.canvas = canvas[0];
-	info.c2.canvas = canvas[1];
-	*/
+	// Set handlers for navbar
+	$(".navbar-right li").click(function(event){
+		$(".navbar-right li").removeClass("active");
+		$(event.target).closest("li").addClass("active");
+		$(".navbar-toggle:visible").click();
+		$(".content, .side").addClass("hidden");
+		var id = $(event.target).closest("li").children("a").attr("href");
+		$(id+"-main, "+id+"-side").removeClass("hidden");
+		$(id+"-main, "+id+"-side").css("opacity","1");
+	});
+	$(".navbar-brand").click(function(event){
+		$("li a[href='#home']").click();
+	});
+	// Fade in effect
+	$("#body").animate({opacity:1},500);
 }
-
 $(document).ready(init);
